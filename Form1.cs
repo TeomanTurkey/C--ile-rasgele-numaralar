a2f336157e16e21ea826bd41622f188ab5797b67
TeomanTurkey <teomanorcan13@gmail.com>
2022-03-21 01:59:40 +0300

    Rastgele sayılar için C# Kodu

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rastgale_Numara;
+namespace Rastgale_Numara
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random r = new Random();
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int randomNumber = r.Next();
+            label1.Text = randomNumber.ToString();
+
+        }
+    }
+}
